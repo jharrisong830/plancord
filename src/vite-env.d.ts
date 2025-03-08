97760f44d0079778141f305d173ac3f8c1cebfda
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-03-08 13:26:48 -0500

    add firebase config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+declare const FIREBASE_API_KEY: string;
+declare const FIREBASE_AUTH_DOMAIN: string;
+declare const FIREBASE_PROJECT_ID: string;
+declare const FIREBASE_STORAGE_BUCKET: string;
+declare const FIREBASE_MESSAGE_SENDER_ID: string;
+declare const FIREBASE_APP_ID: string;
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    define: {
+        FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+        FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        FIREBASE_MESSAGE_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGE_SENDER_ID),
+        FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID)
+    }
 });
